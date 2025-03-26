93a292eb3fda2c7704b63c9978397ce862c4d21d
[min-zu] <[94minju@gmail.com]>
2025-03-26 10:36:00 +0900

    database 로직 수정

--- src/app/db/database.ts
+++ src/app/db/database.ts
@@ -9,6 +9,15 @@ const database = mysql.createPool({
   port: 3306
 })
 
+// 추후 환경변수 사용용
+// const database = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: Number(process.env.DB_PORT) || 3306
+// });
+
 // DB 연결 함수
 export async function getDBConnection() {
   return await database.getConnection();
@@ -16,8 +25,15 @@ export async function getDBConnection() {
 
 // 쿼리 실행 함수
 export async function query(sql: string, values: any[] = []) {
-  const connection = await getDBConnection();
-  const [results] = await connection.execute(sql, values);
-  connection.release();
-  return results;
+  let connection;
+  try {
+    connection = await getDBConnection();
+    const [results] = await connection.execute(sql, values);
+    return results;
+  } catch (error) {
+    console.error("Database Query Error:", error);
+    throw new Error("Database query failed");
+  } finally {
+    if (connection) connection.release();
+  }
 }
